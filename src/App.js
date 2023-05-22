a2a6a8ad13e124ab043d15c848f8983568f69f98
MIN0225 <cro77@naver.com>
2023-05-22 13:16:56 +0900

    ch7 To Do List part Two

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   };
   return (
     <div>
-      <h1>My To Dos({toDos.length})</h1>
+      <h1>My To Dos({toDos.len})</h1>
       <form onSubmit={onSubmit}>
         <input
           onChange={onChange}
@@ -25,6 +25,12 @@ function App() {
         />
         <button>Add To Do</button>
       </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
